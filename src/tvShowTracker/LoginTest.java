e50cc21b9a39be3eb85ed5c5c579b8eac56cffb3
Talha Qureshi <Talha215@users.noreply.github.com>
2023-01-12 12:11:23 -0500

    Added comments

--- src/tvShowTracker/LoginTest.java
+++ src/tvShowTracker/LoginTest.java
@@ -2,12 +2,16 @@ package tvShowTracker;
 
 public class LoginTest {
 	public static void main(String[] args) {
-		Login login = new Login("not", "one");
+		Login login = new Login("test", "notpassword");
 		if(login.ID > 0)
-			System.out.println("Match found with ID: " + login.ID);
+			System.out.println("Match found with ID: " + login.ID); //does not run
 		
-		Login anotherUser = new Login("test", "test");
+		Login anotherUser = new Login("test3", "test3");
 		if(anotherUser.ID > 0)
 			System.out.println("Match found with ID: " + anotherUser.ID);
+		
+		Login lastUser = new Login("test", "test");
+		if(lastUser.ID > 0)
+			System.out.println("Match found with ID: " + lastUser.ID);
 	}
 }
