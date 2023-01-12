44e56e356e6e56317917f6ed8c72bd77422f38b8
Travis Saulat <tjsaulat@gmail.com>
2023-01-12 12:31:08 -0500

    Update ProgressTrackerDB.sql
    Added foreign keys to the users table

--- ProgressTrackerDB.sql
+++ ProgressTrackerDB.sql
@@ -19,11 +19,15 @@ PRIMARY KEY (login_id)
 
 CREATE TABLE users (
 users_id INT auto_increment,
+tvshows_id INT,
+login_id INT,
 show_id INT, 
 show_name VARCHAR(30),
 show_status VARCHAR(10),
 progress INT,
-PRIMARY KEY(users_id)
+PRIMARY KEY(users_id),
+FOREIGN KEY (tvshows_id) references tvshows(tvshows_id),
+FOREIGN KEY (login_id) references login(login_id)
 );
 
 insert into login (login_id, user_name, passcode) values(null, 'user1', 'password1');
