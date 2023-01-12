3e39228b68a1b7836406addfe9b11e47c41dc838
Travis Saulat <tjsaulat@gmail.com>
2023-01-12 12:20:12 -0500

    Update ProgressTrackerDB.sql
    Added several insert (Tvshows and users)

--- ProgressTrackerDB.sql
+++ ProgressTrackerDB.sql
@@ -23,7 +23,20 @@ show_id INT,
 show_name VARCHAR(30),
 show_status VARCHAR(10),
 progress INT,
-primary key(users_id)
+PRIMARY KEY(users_id)
 );
 
+insert into login (login_id, user_name, passcode) values(null, 'user1', 'password1');
+insert into login (login_id, user_name, passcode) values(null, 'user2', 'password2');
+
+insert into tvshows (tvshows_id, tvshow_name) values (null, 'Sopranos');
+insert into tvshows (tvshows_id, tvshow_name) values (null, 'Seinfield');
+insert into tvshows (tvshows_id, tvshow_name) values (null, 'The Twilight Zone');
+insert into tvshows (tvshows_id, tvshow_name) values (null, 'All in the Family');
+insert into tvshows (tvshows_id, tvshow_name) values (null, 'M*A*S*H');
+insert into tvshows (tvshows_id, tvshow_name) values (null, 'The Mary Tyler Moore Show');
+insert into tvshows (tvshows_id, tvshow_name) values (null, 'Mad Men');
+insert into tvshows (tvshows_id, tvshow_name) values (null, 'Cheers');
+insert into tvshows (tvshows_id, tvshow_name) values (null, 'The Wire');
+insert into tvshows (tvshows_id, tvshow_name) values (null, 'The West Wing');
 
