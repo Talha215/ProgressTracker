cb05433f8d6f855cc7d4d06230ee1b1f152a5116
elijahb22 <elibusick11@gmail.com>
2023-01-12 12:00:47 -0500

    added option menu system

--- src/tvShowTracker/menu.java
+++ src/tvShowTracker/menu.java
@@ -1,5 +1,39 @@
 package tvShowTracker;
 
-public class menu {
+import java.util.Scanner;
 
+public class menu {
+	public static void main(String[] args) {
+	menu();
+	}
+	
+	public static void menu() {
+		System.out.println("Welcome to the Tv show Data Tracker");
+		System.out.println("\n 1.Add Show");
+		System.out.println("\n 2.View Progress");
+		System.out.println("\n 3.Update");
+		
+		Scanner sc = new Scanner(System.in);
+	
+		int option = sc.nextInt();
+		sc.nextLine();
+		
+		switch(option){
+			case 1: 
+				System.out.println("Add Show");
+				break;
+			case 2:
+				System.out.println("View Progress");
+				break;
+			case 3:
+				System.out.println("Update");
+				break;
+			
+			
+		}
+		
+		sc.close();
+	}
+
+ 
 }
