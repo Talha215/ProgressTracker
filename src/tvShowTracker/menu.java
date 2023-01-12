eefa9543fb7c94539c7e2d76cbadec93c2b697e4
Talha Qureshi <Talha215@users.noreply.github.com>
2023-01-12 11:10:40 -0500

    Added RunningQueries

--- src/tvShowTracker/menu.java
+++ src/tvShowTracker/menu.java
@@ -1,5 +1,13 @@
 package tvShowTracker;
 
 public class menu {
-
+	
+	
+	public static int checkLogin(String username, String password) {
+		int userID = -1;
+		
+		//check if username and password match in DB
+		
+		return userID;
+	}
 }
